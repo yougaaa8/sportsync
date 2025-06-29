103fac25aa4e073f3c93e47dbcc23b7a6430693d
yougaaa8 <dyoga.chanti89@gmail.com>
2025-06-29 10:28:47 +0700

    add search for user profile by email

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('auth/register/', views.register, name='register'),
+    path('auth/login/', views.login, name='login'),
+    path('auth/logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.update_profile, name='update_profile'),
+    path('get-user-profile/', views.get_user_id_by_email,
+         name='get_user_id_by_email'),
 ]
