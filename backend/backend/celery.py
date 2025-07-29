41b14578c28e83aa208877f1bc85c8a50d102c31
yougaaa8 <dyoga.chanti89@gmail.com>
2025-07-29 16:12:23 +0700

    celery memory optimize

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -6,11 +6,22 @@ app = Celery('backend')
 
 app.conf.update(
     worker_max_memory_per_child=256 * 1024,
-    worker_disable_rate_limits=True,
-
-    worker_max_tasks_per_child=100,
-
+    worker_max_tasks_per_child=50,
     worker_prefetch_multiplier=1,
+
+    worker_concurrency=2,
+
+    worker_disable_rate_limits=True,
+    task_acks_late=True,
+    task_reject_on_worker_lost=True,
+
+    result_expires=1800,
+    result_backend_max_retries=3,
+
+    broker_connection_retry_on_startup=True,
+    broker_connection_retry=True,
+    broker_connection_max_retries=10,
+
 
     # Task routing
     task_routes={
@@ -18,8 +29,6 @@ app.conf.update(
         'notifications.tasks.cleanup_old_notifications': {'queue': 'cleanup'},
         'notifications.tasks.send_single_notification': {'queue': 'notifications'},
     },
-
-    result_expires=3600,
 )
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
